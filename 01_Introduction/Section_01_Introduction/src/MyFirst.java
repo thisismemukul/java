dac94d4fdb9a206b03503d0cec28b3ac253c487d
Mukul Saini <w41k3r.12@gmail.com>
2024-03-16 16:06:58 +0530

    first pass arg[]

--- 01_Introduction/Section_01_Introduction/src/MyFirst.java
+++ 01_Introduction/Section_01_Introduction/src/MyFirst.java
@@ -3,5 +3,6 @@ import java.lang.*;
 public class MyFirst{
     public  static  void  main(String arg[]){
         System.out.println("Hello World! ");
+//        System.out.println(arg[0]);
     }
 }
