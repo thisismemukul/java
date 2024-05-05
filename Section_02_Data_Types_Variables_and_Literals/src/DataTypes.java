07a9b7c5fb23237e8853f7da4727c880b5133931
Mukul Saini <w41k3r.12@gmail.com>
2024-05-05 16:15:35 +0530

    dataTypes

---
+++ Section_02_Data_Types_Variables_and_Literals/src/DataTypes.java
@@ -0,0 +1,7 @@
+public class DataTypes {
+    public static void main(String args[]){
+        System.out.println("Int Max: "+ Integer.MAX_VALUE);
+        System.out.println("Int Min: "+ Integer.MIN_VALUE);
+        System.out.println("Int Bytes: "+ Integer.BYTES);
+    }
+}
