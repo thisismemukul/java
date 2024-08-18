5cddc2ea0f4ec0294186193f5f28ac5ad76f25bf
thisismemukul <w41k3r.12@gmail.com>
2024-08-18 21:38:06 +0530

    final commit

--- Section_12_Inheritance/src/Cylinder.java
+++ Section_12_Inheritance/src/Cylinder.java
@@ -1,42 +1,105 @@
-public class Cylinder extends Circle{
-
+/**
+ * Represents a cylinder with a specific radius and height.
+ */
+public class Cylinder extends Circle {
     private double height;
+
+    /**
+     * Default constructor that initializes the radius and height to 0.
+     */
     public Cylinder() {
         super();
-        this.height=0;
+        this.height = 0;
     }
+
+    /**
+     * Parameterized constructor that initializes the radius and height to the specified values.
+     *
+     * @param radius the radius of the cylinder's base
+     * @param height the height of the cylinder
+     * @throws IllegalArgumentException if the radius or height is negative
+     */
     public Cylinder(double radius, double height) {
         super(radius);
-        setHeight(height);
+        setHeight(height); // Utilize the setter for validation
     }
+
+    /**
+     * Returns the height of the cylinder.
+     *
+     * @return the height of the cylinder
+     */
     public double getHeight() {
         return height;
     }
+
+    /**
+     * Sets the height of the cylinder.
+     *
+     * @param height the new height of the cylinder
+     * @throws IllegalArgumentException if the height is negative
+     */
     public void setHeight(double height) {
-        if(height<0){
-            throw new IllegalArgumentException("Height must be a positive number");
+        if (height < 0) {
+            throw new IllegalArgumentException("Height must be a non-negative number");
         }
-        this.height=height;
+        this.height = height;
     }
+
+    /**
+     * Sets the radius and height of the cylinder.
+     *
+     * @param radius the new radius of the cylinder's base
+     * @param height the new height of the cylinder
+     */
     public void setDimensions(double radius, double height) {
         super.setRadius(radius);
         setHeight(height);
     }
 
-    public double baseArea(){
+    /**
+     * Returns the area of the cylinder's base.
+     *
+     * @return the base area of the cylinder
+     */
+    public double getBaseArea() {
         return super.getArea();
     }
-    public double volume(){
-        return baseArea()*getHeight();
+
+    /**
+     * Calculates the volume of the cylinder.
+     *
+     * @return the volume of the cylinder
+     */
+    public double getVolume() {
+        return getBaseArea() * getHeight();
     }
 
-    public double lateralSurfaceArea(){
+    /**
+     * Calculates the lateral surface area of the cylinder.
+     *
+     * @return the lateral surface area of the cylinder
+     */
+    public double getLateralSurfaceArea() {
         return 2 * Math.PI * getRadius() * getHeight();
     }
 
-    public double surfaceArea() {
-        return (2 * baseArea()) + lateralSurfaceArea();
+    /**
+     * Calculates the total surface area of the cylinder.
+     *
+     * @return the total surface area of the cylinder
+     */
+    public double getSurfaceArea() {
+        return 2 * getBaseArea() + getLateralSurfaceArea();
     }
 
-
+    /**
+     * Returns a string representation of the cylinder.
+     *
+     * @return a string representation of the cylinder
+     */
+    @Override
+    public String toString() {
+        return "Cylinder [radius=" + getRadius() + ", height=" + height + "]";
+    }
 }
--- Section_12_Inheritance/src/Main.java
+++ Section_12_Inheritance/src/Main.java
@@ -4,15 +4,31 @@ public class Main {
 
         Circle circle = new Circle();
         circle.setRadius(2);
+        System.out.println("Circle:");
         System.out.println("Radius: " + circle.getRadius());
         System.out.println("Area: " + circle.getArea());
         System.out.println("Perimeter: " + circle.getPerimeter());
 
         Cylinder cylinder = new Cylinder();
-        cylinder.setRadius(3);
+        cylinder.setDimensions(3, 5); // Set both radius and height
+        System.out.println("Cylinder:");
         System.out.println("Radius: " + cylinder.getRadius());
+        System.out.println("Height: " + cylinder.getHeight());
+        System.out.println("Volume: " + cylinder.getVolume());
+        System.out.println("Surface Area: " + cylinder.getSurfaceArea());
+        System.out.println("Lateral Surface Area: " + cylinder.getLateralSurfaceArea());
         System.out.println(cylinder);
 
+        try {
+            circle.setRadius(-1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
 
+        try {
+            cylinder.setHeight(-1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
     }
 }
