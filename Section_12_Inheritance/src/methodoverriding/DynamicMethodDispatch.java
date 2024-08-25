35ac42529371a88cc919b0c0c363f5114fc8d02b
thisismemukul <w41k3r.12@gmail.com>
2024-08-26 00:20:19 +0530

    dynamic method dispatch notes

--- Section_12_Inheritance/src/methodoverriding/DynamicMethodDispatch.java
+++ Section_12_Inheritance/src/methodoverriding/DynamicMethodDispatch.java
@@ -1,41 +1,48 @@
 package methodoverriding;
 
-class AnimalSuper{
-    public void sound(){
+// Base class
+class AnimalSuper {
+    // Method to be overridden by subclasses
+    public void sound() {
         System.out.println("AnimalSuper sound");
     }
 }
 
-class DogSub extends AnimalSuper{
-    public void sound(){
+// Subclass representing a Dog
+class DogSub extends AnimalSuper {
+    // Overriding the sound method to provide Dog-specific behavior
+    @Override
+    public void sound() {
         System.out.println("DogSub sounds like Bark");
     }
 }
 
-class CatSub extends AnimalSuper{
-    public void sound(){
+// Subclass representing a Cat
+class CatSub extends AnimalSuper {
+    // Overriding the sound method to provide Cat-specific behavior
+    @Override
+    public void sound() {
         System.out.println("CatSub sounds like Meow");
     }
 }
 
-
-
 public class DynamicMethodDispatch {
-            public static void main(String[] args) {
-                AnimalSuper animal = new AnimalSuper();
-                animal.sound();
-                DogSub dog = new DogSub();
-                dog.sound();
-                CatSub cat = new CatSub();
-                cat.sound();
-                
+    public static void main(String[] args) {
+        // Creating instances of each class
+        AnimalSuper animal = new AnimalSuper();
+        animal.sound(); // Calls the sound method of AnimalSuper
 
-                animal = new DogSub();
-                animal.sound();
-                animal = new CatSub();
-                animal.sound();
+        DogSub dog = new DogSub();
+        dog.sound(); // Calls the sound method of DogSub
 
+        CatSub cat = new CatSub();
+        cat.sound(); // Calls the sound method of CatSub
 
-            }
+        // Demonstrating dynamic method dispatch
+        animal = new DogSub(); // animal now refers to a DogSub instance
+        animal.sound(); // Calls the sound method of DogSub due to dynamic dispatch
 
+        animal = new CatSub(); // animal now refers to a CatSub instance
+        animal.sound(); // Calls the sound method of CatSub due to dynamic dispatch
+    }
 }
