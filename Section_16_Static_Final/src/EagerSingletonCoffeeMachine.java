62f966f6dcdcbd12e3a216779b7cb1e8c49bddb0
thisismemukul <w41k3r.12@gmail.com>
2024-09-07 23:54:32 +0530

    BillPughSingleton

--- Section_16_Static_Final/src/EagerSingletonCoffeeMachine.java
+++ Section_16_Static_Final/src/EagerSingletonCoffeeMachine.java
@@ -22,3 +22,14 @@ public class EagerSingletonCoffeeMachine {
         System.out.println("Serving coffee.");
     }
 }
+class BillPughSingleton {
+    private BillPughSingleton() {}
+
+    private static class SingletonHelper {
+        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
+
+    public static BillPughSingleton getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+}
